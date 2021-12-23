ee80fa428c528b5b92e9530c9344261953d37dbf
Park ChanJong <7000cj@gmail.com>
2021-12-23 19:08:36 +0900

    QueryProjection

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -11,6 +11,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 import study.querydsl.dto.MemberDto;
+import study.querydsl.dto.QMemberDto;
 import study.querydsl.entity.Member;
 import study.querydsl.entity.QMember;
 import study.querydsl.entity.Team;
@@ -359,6 +360,17 @@ public class QuerydslBasicTest {
 
         for (MemberDto memberDto : result) {
             System.out.println("memberDto = " + memberDto);
+        }
+    }
+
+    @Test
+    public void findDtoByQueryProjection() throws Exception {
+        List<MemberDto> result = queryFactory.select(new QMemberDto(member.username, member.age))
+                                             .from(member)
+                                             .fetch();
+
+        for (MemberDto memberDto : result) {
+            System.out.println(memberDto);
         }
     }
 }
