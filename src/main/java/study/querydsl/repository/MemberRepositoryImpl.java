b2fb7a69bf2eac2fbbc25e78045e50d26d2f2fda
Park ChanJong <7000cj@gmail.com>
2021-12-29 00:13:19 +0900

    custom repo

---
+++ src/main/java/study/querydsl/repository/MemberRepositoryImpl.java
@@ -0,0 +1,51 @@
+package study.querydsl.repository;
+
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import study.querydsl.dto.MemberSearchCondition;
+import study.querydsl.dto.MemberTeamDto;
+import study.querydsl.dto.QMemberTeamDto;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+import static org.springframework.util.ObjectUtils.isEmpty;
+import static study.querydsl.entity.QMember.member;
+import static study.querydsl.entity.QTeam.team;
+
+public class MemberRepositoryImpl implements MemberRepositoryCustom {
+
+    private final JPAQueryFactory queryFactory;
+
+    public MemberRepositoryImpl(EntityManager em) {
+        this.queryFactory = new JPAQueryFactory(em);
+    }
+
+    @Override
+    public List<MemberTeamDto> search(MemberSearchCondition condition) {
+        return queryFactory.select(new QMemberTeamDto(member.id, member.username, member.age, team.id, team.name))
+                           .from(member)
+                           .leftJoin(member.team, team)
+                           .where(usernameEq(condition.getUsername()),
+                                  teamNameEq(condition.getTeamName()),
+                                  ageGoe(condition.getAgeGoe()),
+                                  ageLoe(condition.getAgeLoe()))
+                           .fetch();
+    }
+
+    private BooleanExpression usernameEq(String username) {
+        return isEmpty(username) ? null : member.username.eq(username);
+    }
+
+    private BooleanExpression teamNameEq(String teamName) {
+        return isEmpty(teamName) ? null : team.name.eq(teamName);
+    }
+
+    private BooleanExpression ageGoe(Integer ageGoe) {
+        return ageGoe == null ? null : member.age.goe(ageGoe);
+    }
+
+    private BooleanExpression ageLoe(Integer ageLoe) {
+        return ageLoe == null ? null : member.age.loe(ageLoe);
+    }
+}
