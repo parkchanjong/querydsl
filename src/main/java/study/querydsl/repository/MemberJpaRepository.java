9e2707e1fbd9076ffcc8dab2a55486b32679d34e
Park ChanJong <7000cj@gmail.com>
2021-12-28 17:45:27 +0900

    동적쿼리 빌더사용

--- src/main/java/study/querydsl/repository/MemberJpaRepository.java
+++ src/main/java/study/querydsl/repository/MemberJpaRepository.java
@@ -1,9 +1,14 @@
 package study.querydsl.repository;
 
+import com.querydsl.core.BooleanBuilder;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.stereotype.Repository;
+import study.querydsl.dto.MemberSearchCondition;
+import study.querydsl.dto.MemberTeamDto;
+import study.querydsl.dto.QMemberTeamDto;
 import study.querydsl.entity.Member;
 
+import static org.springframework.util.StringUtils.hasText;
 import static study.querydsl.entity.QMember.member;
 import static study.querydsl.entity.QTeam.team;
 
@@ -50,6 +55,27 @@ public class MemberJpaRepository {
     public List<Member> findByUsername_Querydsl(String username) {
         return queryFactory.selectFrom(member)
                            .where(member.username.eq(username))
+                           .fetch();
+    }
+
+    public List<MemberTeamDto> searchByBuilder(MemberSearchCondition condition) {
+        BooleanBuilder builder = new BooleanBuilder();
+        if (hasText(condition.getUsername())) {
+            builder.and(member.username.eq(condition.getUsername()));
+        }
+        if (hasText(condition.getTeamName())) {
+            builder.and(team.name.eq(condition.getTeamName()));
+        }
+        if (condition.getAgeGoe() != null) {
+            builder.and(member.age.goe(condition.getAgeGoe()));
+        }
+        if (condition.getAgeLoe() != null) {
+            builder.and(member.age.loe(condition.getAgeLoe()));
+        }
+        return queryFactory.select(new QMemberTeamDto(member.id, member.username, member.age, team.id, team.name))
+                           .from(member)
+                           .leftJoin(member.team, team)
+                           .where(builder)
                            .fetch();
     }
 }
